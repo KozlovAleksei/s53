c6e763c58bac24e3904acac39ff35fe50d00dd1e
Aleksei Kozlov <awkozlov.mba@gmail.com>
2023-10-08 12:37:19 +0300

    7

--- mlb/main.py
+++ mlb/main.py
@@ -1,3 +1,6 @@
+from binance.spot import Spot  # pip install binance-connector
+from binance.error import ClientError
+
 import time
 import mlb
 import os
@@ -5,6 +8,8 @@ import os
 print('start')
 
 gNetRoot = ''
+symbol = "BTCUSDT"
+interval = "10m"
 
 def main(test):
     if test:
@@ -12,13 +17,19 @@ def main(test):
         base_url = 'https://testnet.binance.vision'  # Ссылка на тестовую документацию
         api_key = mlb.read_text_from_file(os.path.join('C:\\', 'keys', 't_api_key.txt'))
         api_secret = mlb.read_text_from_file(os.path.join('C:\\', 'keys', 't_api_secret.txt'))
-        gNetRoot = os.path.join('Z:\\', '#TEST')
+        gNetRoot = os.path.join('C:\\', '#TEST')
     else:
         # В реальном режиме
         base_url = 'https://api2.binance.com'  # Ссылка на документацию
         api_key = mlb.read_text_from_file(os.path.join('C:\\', 'keys', 'r_api_key.txt'))
         api_secret = mlb.read_text_from_file(os.path.join('C:\\', 'keys', 'r_api_secret.txt'))
-        gNetRoot = os.path.join('Z:\\', '#REAL')
+        gNetRoot = os.path.join('C:\\', '#REAL')
+
+    if not os.path.exists(gNetRoot):
+        os.makedirs(gNetRoot)
+
+    cl = Spot(api_key=api_key, api_secret=api_secret, base_url=base_url)
+    print('base_url=', base_url)
 
     print('gNetRoot=', gNetRoot)
     while True:
