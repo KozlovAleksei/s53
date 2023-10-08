8822a9097c50f318c5ddff5a78f95320d025c7d9
Aleksei Kozlov <awkozlov.mba@gmail.com>
2023-10-08 11:00:31 +0300

    1

---
+++ mlb/main.py
@@ -0,0 +1,4 @@
+# test
+base_url = 'https://testnet.binance.vision'  # https://youtu.be/NczNjVxwx3A?t=636
+api_key = 'LDt6vLKawn1VYaAs8CTwYx6RtAiVrowVe3wu1bRerko9VZ0HAr2gL7KMMS3kM5DH'
+api_secret = 'nvXZgMuLaQNlbGO8STgdzMYixrhuPEwPL641F3AXwelU9LgqRDz4iYYyoWSxZDRE'
